a51c4c1782eaa296f368b57dff6ec97e55d51c06
Markus Felten <Markus.Felten@gmx.de>
2022-07-20 15:23:01 +0200

    fix: add name + version

--- tests/app/src/App.svelte
+++ tests/app/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { EntitlementBadges, Entitlement } from "../../../src/index.svelte";
+  import {name,version} from "./constants.mjs";
 
   const e1 = new Entitlement({ cn: "e1" });
   const e2 = new Entitlement("e2");
@@ -9,7 +10,7 @@
   const entitlements2 = [e3];
 </script>
 
-<h1>Example</h1>
+<h1>{name} @{version}</h1>
 <EntitlementBadges {entitlements} />
 Into:
 <EntitlementBadges entitlements={entitlements2} />
