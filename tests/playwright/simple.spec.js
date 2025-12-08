aab26ee0d142d1213a4904dfde7575d5316c58a6
Markus Felten <Markus.Felten@gmx.de>
2025-12-08 11:00:13 +0000

    tesT: fix playwright assertion

--- tests/playwright/simple.spec.js
+++ tests/playwright/simple.spec.js
@@ -4,5 +4,5 @@ import { test, expect } from "@playwright/test";
 test("log canvas present", async ({ page }) => {
   await page.goto("http://localhost:5173/");
 
-  await expect(page.locator(".entitlement")).toContainText("e1");
+  await expect(page.getByText("e1")).toBeVisible();
 });
